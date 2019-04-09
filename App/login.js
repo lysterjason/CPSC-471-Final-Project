6dca944f323373ba700db65ffbfc25f27363e4fb
lysterjason <jason.lyster@ucalgary.ca>
2019-04-09 16:00:32 -0600

    First database work

---
+++ App/login.js
@@ -0,0 +1,85 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+var con = mysql.createConnection({
+  host: "10.13.163.141",
+  user: "jasonl",
+	password: "password",
+	database: "project"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM user", function (err, result, fields) {
+    if (err) throw err;
+		console.log(result);
+  });
+});
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+
+	if (username && password) {
+		con.query('SELECT * FROM user WHERE USERNAME = ? AND PASSWORD = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/goHome');
+			} else {
+			}			
+			response.end();
+		});
+	} else {
+		response.end();
+	}
+});
+
+app.post('/register', function(request, response) {
+	var username = request.body.username;
+	var email = request.body.email;
+	var fname = request.body.firstName;
+	var lname = request.body.lastName;
+	var password = request.body.password;
+	var confirmpassword = request.body.confirmpassword;
+
+	var VALUES = [null, username, password, Date.now(), fname, lname, null, email, null];
+	var sql = "INSERT INTO user (ID, USERNAME, EMAIL, FIRST_NAME, LAST_NAME, PASSWORD, SCHOOL_ID) VALUES ?";
+	con.query(sql, [VALUES], function(error, result) {
+		console.log(result);
+	});
+});
+
+app.get('/goLogin', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.get('/goHome', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/register', function(request, response) {
+	response.sendFile(path.join(__dirname + '/register.html'));
+});
+
+app.listen(3000, function() {
+	console.log('Server is running on port 3000...');
+});
